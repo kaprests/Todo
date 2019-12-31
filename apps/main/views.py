6810ea66a396c0751bdb0c5bdf5afcb1859f77fe
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-12-31 07:32:16 +0100

    Removes unused code from views.py

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,18 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, FormView
-from django.http import JsonResponse, HttpResponse
-from django.core.serializers import serialize
 
-from rest_framework import viewsets
-from .serializers import TodoSerializer
-
-from.models import Todo
-
-class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-
-
-def get_data(request):
-    data = serialize('json', Todo.objects.all())
-    return HttpResponse(data, content_type='application/json')
+# Views goes here :)
